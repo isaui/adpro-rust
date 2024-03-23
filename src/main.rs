1e110ffa1b566b5f433bfbdc21c481b3553fdaeb
isaui <isadestroyed17@gmail.com>
2024-03-23 10:17:49 +0700

    (4) Simulation of slow request.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     fs,
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream}
+    net::{TcpListener, TcpStream}, thread, time::Duration
 };
 
 fn main() {
@@ -22,6 +22,9 @@ fn handle_connection(mut stream: TcpStream) {
     let http_request: Vec<_> = buf_reader.lines().map(|result| result.unwrap()).take_while(|line| !line.is_empty()).collect();
     
     let (status_line, contents) = if !http_request.is_empty() && http_request[0].starts_with("GET / ") {
+        ("HTTP/1.1 200 OK", fs::read_to_string("src/templates/hello.html").unwrap_or_else(|_| String::from("File not found")))
+    } else if !http_request.is_empty() && http_request[0].starts_with("GET /sleep "){
+        thread::sleep(Duration::from_secs(10));
         ("HTTP/1.1 200 OK", fs::read_to_string("src/templates/hello.html").unwrap_or_else(|_| String::from("File not found")))
     } else {
         ("HTTP/1.1 404 NOT FOUND", fs::read_to_string("src/templates/404.html").unwrap_or_else(|_| String::from("404 Not Found")))
