961c0e22a83ee1478e97cbd1d190f75e25307aad
isaui <isadestroyed17@gmail.com>
2024-03-23 09:21:17 +0700

    feat: add 404 page and its route

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,13 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new
     (&mut stream);
-    let _http_request: Vec<_> = buf_reader.lines().
-    map(|result |
-        result.unwrap()).take_while(|line| 
-            !line.is_empty()).collect();
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("src/templates/hello.html").unwrap();
+    let http_request: Vec<_> = buf_reader.lines().map(|result| result.unwrap()).take_while(|line| !line.is_empty()).collect();
+    
+    let (status_line, contents) = if !http_request.is_empty() && http_request[0].starts_with("GET / ") {
+        ("HTTP/1.1 200 OK", fs::read_to_string("src/templates/hello.html").unwrap_or_else(|_| String::from("File not found")))
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", fs::read_to_string("src/templates/404.html").unwrap_or_else(|_| String::from("404 Not Found")))
+    };
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length:{length}\r\n\r\n{contents}");
     let _res = stream.write_all(
